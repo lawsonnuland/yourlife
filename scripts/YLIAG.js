c8ca2806c02ce917473edef9e13b862a3e1008d6
Lawson Nuland <lawson.nuland@gmail.com>
2021-09-17 21:47:41 -0400

    Updated js

--- scripts/YLIAG.js
+++ scripts/YLIAG.js
@@ -2,19 +2,31 @@ var sadness = 0;
 var misery = 0;
 var emptiness = 0;
 var v0id = 0;
-var miscostmult =1;
+var money = 0;
+var miseryCostMultiplier = 1;
+var emptinessCostMultiplier = 1;
 
 function work() {
-	sadness++;
+	sadness += 1+(misery*.1)+(emptiness*.2);
 	document.getElementById("sadness").innerHTML = sadness;
 }
 
 function buymisery() {
-	if (sadness>=10*(miscostmult)) {
-		sadness -= 10*(miscostmult);
+	if (sadness>=10*(miseryCostMultiplier)) {
+		sadness -= 10*(miseryCostMultiplier);
 		document.getElementById("sadness").innerHTML = sadness;
 		misery++;
-		miscostmult += .2;
+		miseryCostMultiplier += .1;
 		document.getElementById("misery").innerHTML = misery;
+	}
+}
+
+function buyemptiness() {
+	if (misery>=10*(emptinessCostMultiplier)) {
+		misery -= 10*(emptinessCostMultiplier);
+		document.getElementById("misery").innerHTML = sadness;
+		emptiness++;
+		emptinessCostMultiplier += .1;
+		document.getElementById("emptiness").innerHTML = misery;
 	}
 }
