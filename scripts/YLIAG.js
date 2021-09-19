ee7272253d96e605871170a0d9c4589b8a06d236
Lawson Nuland <lawson.nuland@gmail.com>
2021-09-19 16:15:36 -0400

    Finishing Touches
    Finished game, added ability to achieve Peace.

--- scripts/YLIAG.js
+++ scripts/YLIAG.js
@@ -3,14 +3,23 @@ var misery = 0;
 var emptiness = 0;
 var v0id = 0;
 var money = 0;
+var plant = 0;
+var joy = 0;
+var peace = 0;
 var miseryCostMultiplier = 1;
 var emptinessCostMultiplier = 1;
+var plantCostMultiplier = 1;
+var joyCostMultiplier = 1;
 var reset = 1;
 
 function work() {
 	sadness += (10+(misery*1)+(emptiness*2))*reset;
 	document.getElementById("sadness").innerHTML = sadness;
 	document.getElementById("log").innerHTML = "Work sucks, so sadness will increase!";
+	if (reset>2){
+		money += 5*reset;
+		document.getElementById("money").innerHTML = money;
+	}
 }
 
 function buyMisery() {
@@ -46,6 +55,8 @@ function buyVoid() {
 		document.getElementById("emptiness").innerHTML = emptiness;
 		document.getElementById("log").innerHTML = "Boundary condition exceeded. Sadness reset.";
 		reset++;
+		miseryCostMultiplier = 1;
+		emptinessCostMultiplier = 1;
 	}
 	else if (emptiness >=10 && reset == 2) {
 		sadness = 0;
@@ -56,5 +67,52 @@ function buyVoid() {
 		document.getElementById("emptiness").innerHTML = emptiness;
 		document.getElementById("log").innerHTML = "This is weird. I'm not sad anymore? What could this mean? Sadness reset.";
 		reset++;
+		document.getElementById("secretStats").style.display = "block";
+		document.getElementById("secretPurchases").style.display = "block";
+		miseryCostMultiplier = 1;
+		emptinessCostMultiplier = 1;
+	}
+	v0id++;
+	document.getElementById("void").innerHTML = v0id;
+	
+}
+
+function buyPlant() {
+	if (money >= (10*plantCostMultiplier)){
+		money -= 10*(plantCostMultiplier);
+		plantCostMultiplier = 1 + (plant*.1) ;
+		plant++;
+		document.getElementById("money").innerHTML = money;
+		document.getElementById("plant").innerHTML = plant;
+		document.getElementById("log").innerHTML = "A plant seems like a good investment. It'll freshen things up some.";
+	}
+}
+
+function buyJoy() {
+	if (plant >=10*(joyCostMultiplier)) {
+		plant -= 10*(joyCostMultiplier);
+		joyCostMultiplier = 1 + (joy*.1) ;
+		document.getElementById("plant").innerHTML = plant;
+		joy++;
+		joyCostMultiplier = 1 + (joy*.1) ;
+		document.getElementById("joy").innerHTML = joy;
+		document.getElementById("log").innerHTML = "All these plants are taking your mind off of the stressors of your life. Was this the answer all along?";
+	}
+}
+
+function buyPeace() {
+	if (joy >=10) {
+		joy -= 10;
+		joyCostMultiplier = 1;
+		document.getElementById("joy").innerHTML = joy;
+		peace++;
+		sadness = 0;
+		misery =0;
+		emptiness = 0;
+		document.getElementById("sadness").innerHTML = sadness;
+		document.getElementById("misery").innerHTML = misery;
+		document.getElementById("emptiness").innerHTML = emptiness;
+		document.getElementById("peace").innerHTML = peace;
+		document.getElementById("log").innerHTML = "You have so many plants now, that it seems you no longer have time to be sad! You beat the game!";
 	}
 }
